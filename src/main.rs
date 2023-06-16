664f53652e8785049dde903b5c91478ff85b0070
muz <muiz-erem@gmx.de>
2023-06-16 16:57:51 +0200

    sending rays

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-#![allow(dead_code)]
-
 pub mod utility;
 
 use std::io::Write;
 
-const IMAGE_WIDTH: usize = 256;
-const IMAGE_HEIGHT: usize = 256;
+use crate::utility::{
+    ray::Ray,
+    vec3::{ray_color, Vec3},
+};
+
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: usize = 400;
+const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
+
+const VIEWPORT_HEIGHT: f64 = 2.0;
+const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f64 = 1.0;
 
 fn main() {
+    let origin = Vec3::default();
+    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
     println!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255");
 
     (0..IMAGE_HEIGHT).for_each(|j| {
@@ -15,11 +29,13 @@ fn main() {
         eprint!("\rScanlines remaining: {j} ");
         std::io::stderr().flush().unwrap();
         (0..IMAGE_WIDTH).for_each(|i| {
-            let pixel_color = utility::vec3::Vec3 {
-                x: (i as f64) / (IMAGE_WIDTH - 1) as f64,
-                y: (j as f64) / (IMAGE_HEIGHT - 1) as f64,
-                z: 0.25,
+            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let v = j as f64 / (IMAGE_WIDTH - 1) as f64;
+            let ray = Ray {
+                origin,
+                direction: lower_left_corner + horizontal * u + vertical * v - origin,
             };
+            let pixel_color = ray_color(ray);
 
             pixel_color.write_color();
         });
--- src/utility/vec3.rs
+++ src/utility/vec3.rs
@@ -1,3 +1,8 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+use super::ray::Ray;
+
+#[derive(Default, PartialEq, Clone, Copy)]
 pub struct Vec3 {
     pub x: f64,
     pub y: f64,
@@ -5,6 +10,18 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn all(val: f64) -> Self {
+        Self {
+            x: val,
+            y: val,
+            z: val,
+        }
+    }
+
     pub fn write_color(&self) {
         println!(
             "{} {} {}",
@@ -13,4 +30,65 @@ impl Vec3 {
             (255.999 * self.z) as usize
         );
     }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    fn length_squared(&self) -> f64 {
+        self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
+    }
+
+    pub fn unit_vec(&self) -> Vec3 {
+        *self / self.length()
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        if rhs == 0.0 {
+            panic!("cannot divide by 0");
+        }
+        self::Vec3::new(self.x / rhs, self.y / rhs, self.z / rhs)
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        self::Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self::Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self::Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        self::Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+pub fn ray_color(r: Ray) -> Vec3 {
+    let unit_direction = r.direction.unit_vec();
+    let t = 0.5 * (unit_direction.y + 1.0);
+    Vec3::all(1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
